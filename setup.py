4f65635d92424ae0b2ca9ddd70885e487b122e4d
Robinson Bernand <robinson.bernand@arkena.com>
2017-04-14 16:00:36 +0200

    basic packaging

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# SAM
+# Copyright © 2017 Arkena S.A
+
+import computor
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='computorv2',
+    version=computor.__version__,
+    description='ComputorV2 Mega FUN',
+    long_description="""
+    Awesome calc tool
+    """,
+    author='Robinson.bernand',
+    author_email='robinson.bernand@student.42.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=open('requirements.txt').readlines(),
+    tests_require=[
+        'pytest',
+    ],
+    classifiers=[
+        'Operating System :: *',
+        'Programming Language :: Python :: 3',
+    ],
+)
